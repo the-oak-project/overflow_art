d4e070886d584ce4a0b4fcdc0a913de90ba07848
Leo Buckey <jamesabuckey@gmail.com>
2022-03-16 09:37:46 -0400

    fix: correct sytax error and update listening port to 8080

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const PORT = 8080
 
+// App
+const app = express()
 app.get('/', (req, res) => {
 	res.send('Hello World!')
 })
 
-app.listen(port, () => {
-	console.log('Example app listening on port ${port}')
+app.listen(PORT, () => {
+	console.log(`Example app listening on port: ${PORT}`)
 })
