db16de122fea809299243e8f711993211892d523
Leo Buckey <jamesabuckey@gmail.com>
2022-04-26 09:31:53 -0400

    feat: add artwork-details as the landing page for overflow.art

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ArtworkDetailsComponent } from './artwork-details/artwork-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtworkDetailsComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ArtworkDetailsComponent]
 })
 export class AppModule { }
---
+++ app_public/src/app/artwork-details/artwork-details.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artwork-details',
+  templateUrl: './artwork-details.component.html',
+  styleUrls: ['./artwork-details.component.css']
+})
+export class ArtworkDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  public s3Url: string = 'https://the-art-project.s3.us-east-2.amazonaws.com/';
+  public message: string | undefined;
+
+  private showError(error: any): void {
+    this.message = error.message;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
